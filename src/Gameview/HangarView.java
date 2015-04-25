ea945b7ed4b8fd1efaf5d03ff71e5678583dfe35
loictessier <loictessier.inge@gmail.com>
2015-04-25 18:39:17 -0400

    Premiere version fenetre hangar avec bug

--- src/Gameview/HangarView.java
+++ src/Gameview/HangarView.java
@@ -1,5 +1,61 @@
 package Gameview;
 
-public class HangarView {
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Paths;
 
+import org.jsfml.graphics.ConstTexture;
+import org.jsfml.graphics.Image;
+import org.jsfml.graphics.RenderWindow;
+import org.jsfml.graphics.Sprite;
+import org.jsfml.graphics.Texture;
+import org.jsfml.window.VideoMode;
+import org.jsfml.window.event.Event;
+import org.jsfml.window.event.Event.Type;
+
+public class HangarView {
+	
+	RenderWindow HangarWindow;
+	
+	public HangarView()
+	{
+		HangarWindow = new RenderWindow(new VideoMode(800, 600, 32), "Hangar");
+	}
+	
+	public void run(){
+		Texture image = new Texture();
+		Sprite sprite = new Sprite();
+		
+		try
+		{
+			image.loadFromFile(Paths.get("rsc\\Stronghold_tags_8-bit_Middle_Finger.png"));
+		}
+		catch(FileNotFoundException e1)
+		{
+			System.out.println(e1.getMessage());
+		} 
+		catch (IOException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
+		
+		sprite.setTexture(image);
+		HangarWindow.draw(sprite);
+		
+		while(HangarWindow.isOpen())
+		{
+			for(Event event : HangarWindow.pollEvents())
+			{
+				if(event.type == Type.CLOSED){
+					HangarWindow.close();
+				}
+				
+				HangarWindow.clear();
+				
+				HangarWindow.display();
+			}
+		}
+		
+		
+	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
 
+import Gameview.HangarView;
 import controller.Game;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		Game P = new Game();
-		P.start();
+		//Game P = new Game();
+		//P.start();
+		
+		HangarView monHangar = new HangarView();
+		
+		monHangar.run();
 	}
 
 }
